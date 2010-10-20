9faccd0a1ae588ea6ca0272359aef6e1811e598d
Grzegorz Lipski <czacha.osw@gmail.com>
2010-10-20 15:14:22 +0200

    poprawka

--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -1,17 +1,25 @@
 class WorkersController < ApplicationController
+   
+   def index
+    @firm = Firm.find(params[:firm_id])
+    @workers = @firm.workers
+   end
+   
+   def new
+    @firm = Firm.find(params[:firm_id])
+    @worker = @firm.workers.build
+   end
   
-  def new
-    @firm = Firm.find(params[:id])
-    @worker = @firm.workers.build 
-  end
-  
-  def create
-    @firm = Firm.find(params[:id])
+   def create
+    @firm = Firm.find(params[:firm_id])
     @worker = @firm.workers.build(params[:worker])
-    @worker.save
+    @worker.save    
   end
   
-  def destroy
-
-  end
-end
+   def destroy
+      @firm = Firm.find(params[:firm_id]) 
+      @worker = Worker.find(params[:id]) 
+      @worker.destroy 
+      redirect_to firm_workers_path(@firm) 
+   end
+end 
